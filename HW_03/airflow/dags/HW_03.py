4fecedd7fa8806edd0ae39078b1878d4c8909978
Artem Pixaykin <53806715+boolikefoo@users.noreply.github.com>
2023-08-16 20:18:15 +0500

    update

--- HW_03/airflow/dags/HW_03.py
+++ HW_03/airflow/dags/HW_03.py
@@ -4,15 +4,10 @@ import logging
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.python import PythonOperator
-
 import pandas as pd
 import sqlite3
 from zipfile import ZipFile
 import wget
-import logging
-
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
@@ -24,11 +19,11 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-
-# Настройки логера
-logging.config.fileConfig(fname='configs/logger.conf', disable_existing_loggers=False)
-logger = logging.getLogger('customLogger')
-
+# Настройки логгера
+logging.basicConfig(filename='HW_03.log', 
+                    filemode='a',
+                    encoding='utf-8', 
+                    level=logging.DEBUG)
 
 # Функция загрузки файла
 def data_downlo():
@@ -36,12 +31,35 @@ def data_downlo():
 
 
 with DAG(
-    dag_id='MIPT_FINAL_HW',
+    dag_id='MIPT_FINAL_HW_2',
     default_args=default_args,
     description='This is final home work',
     start_date=datetime(2023, 8, 11),
-    schedule_interval='@daily'
+    schedule_interval='@daily
 ) as dag:
+
+    # Создаём базу данных
+    @task(task_id='create_db')
+    def create_db():
+        # Создаём базу данных
+        connection = sqlite3.connect('hw1.db')
+
+        cursor = connection.cursor()
+
+        # Создаём таблицу
+        create_okved_table = """
+        CREATE TABLE IF NOT EXISTS okved(
+        code TEXT,
+        parent_code TEXT,
+        section CHAR,
+        name TEXT,
+        comment TEXT
+        )
+        """
+
+        cursor.execute(create_okved_table)
+
+        connection.commit()
 
     # Загружаем файл егрюл и разархиваруем данные
     @task(task_id='data_downloading')
@@ -60,20 +78,12 @@ with DAG(
 
     run_task0 = data_downloading('someurl')
 
-    task1 = PythonOperator(
-        task_id='first_task',
-        python_callable=testdef
+    task_1 = PythonOperator(
+        task_id='data_downloading',
+        python_callable=data_downloading
     )
 
-    task2 = PythonOperator(
-        task_id='second_task',
-        python_callable=testdef
-    )
 
-    task3 = PythonOperator(
-        task_id='thrid_task',
-        python_callable=testdef
-    )
 
     # Task dependency method 1
     # task1.set_downstream(task2)
@@ -83,8 +93,6 @@ with DAG(
     # task1 >> task2
     # task1 >> task3
 
-    run >> task3 >> task2
-    
-
+    run_task0 >> task_1
     # Task dependency method 3
     # task1 >> [task2, task3]
