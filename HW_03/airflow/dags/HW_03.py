e3f3e94f995276143c0eebffeb65e71ecd51a332
Artem Pixaykin <53806715+boolikefoo@users.noreply.github.com>
2023-08-17 12:26:05 +0500

    update

--- HW_03/airflow/dags/HW_03.py
+++ HW_03/airflow/dags/HW_03.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 from airflow.operators.python import PythonOperator
 import pandas as pd
 import sqlite3
-from zipfile import ZipFile
+import zipfile
 import wget
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import Session
@@ -19,6 +19,16 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
+
+# параметры для парсинга
+user_agent = {'User-agent': 'Mozilla/5.0'}
+url_params = {
+    'text': 'middle python developer',
+    'area': '1',
+    'per_page': 100,
+    'page': 0
+}
+
 # Настройки логгера
 logging.basicConfig(filename='HW_03.log', 
                     filemode='a',
@@ -26,8 +36,91 @@ logging.basicConfig(filename='HW_03.log',
                     level=logging.DEBUG)
 
 # Функция загрузки файла
-def data_downlo():
-    print("this is test function")
+def run_data_downloading():
+    # загружаем файл
+    try:
+        url = 'https://ofdata.ru/open-data/download/egrul.json.zip'
+        filename = wget.download(url)
+        logging.info('Файл загружен')
+    except Exception as e:
+        logging.error(f'Не удаётся загрузить файл: {e}')
+
+
+# функция выбора компаний по оквэд
+def run_extract_okved():
+    # Создаём подключение к DB
+    connection = sqlite3.connect('HW1.db')
+
+    # Создаём курсор
+    cursor = connection.cursor()
+
+    # открываем zip архив
+    with zipfile.ZipFile(targetFile, 'r') as zipobj:
+        # получаем список файлов в архиве
+        fileList = zipobj.namelist()
+        # Читаем данные из файлов архива по списку
+        for name in fileList:
+            with zipobj.open(name) as file:
+                #  десюриализация json
+                jfile = json.load(file)
+
+                # Создаём развёрнутый DataFrame из json
+                dfn = pd.json_normalize(jfile)
+
+                try:
+                    # Переименовываем столбец с основным видом деятельности
+                    dfn.rename(columns={'data.СвОКВЭД.СвОКВЭДОсн.КодОКВЭД': 'okved'}, inplace=True)
+                    # Удаляем NAN строки из фрейма
+                    dfn = dfn.dropna(subset=['okved'])
+                    # Фильтруем данные по оквэд
+                    dfn_filtred = dfn[dfn['okved'].str.contains('^61')][['name', 'inn', 'full_name', 'okved']]
+                    # Записываем данные в DB
+                    dfn_filtred.to_sql('telecom_companies', connection, if_exists='append', index=False)
+
+                except Exception as e:
+                    logging.error(f'Произошла ошибка во время работы: {e} в файле {file}')
+
+    # закрываем курсор
+    cursor.close()
+
+    # Закрываем соединение
+    connection.close()
+
+# Функция создания баз данных
+def run_create_db():
+    # Создаём базу данных
+    connection = sqlite3.connect('hw1.db')
+
+    cursor = connection.cursor()
+
+    # Создаём таблицу
+    create_telecom_companies_table = """
+    CREATE TABLE IF NOT EXISTS telecom_companies(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        inn INTEGER,
+        full_name TEXT,
+        okved TEXT
+        )
+    """
+    cursor.execute(create_vacancies_table)
+
+    create_vacancies_table = """
+    CREATE TABLE IF NOT EXISTS vacancies(
+    id INTAGE,
+    company_name TEXT,
+    position TEXT,
+    job_description TEXT,
+    key_skills TEXT
+    )
+
+    """
+    cursor.execute(create_vacancies_table)
+
+    connection.commit()
+
+
+# Функция парсинга вакансий
 
 
 with DAG(
@@ -39,60 +132,30 @@ with DAG(
 ) as dag:
 
     # Создаём базу данных
-    @task(task_id='create_db')
-    def create_db():
-        # Создаём базу данных
-        connection = sqlite3.connect('hw1.db')
-
-        cursor = connection.cursor()
-
-        # Создаём таблицу
-        create_okved_table = """
-        CREATE TABLE IF NOT EXISTS okved(
-        code TEXT,
-        parent_code TEXT,
-        section CHAR,
-        name TEXT,
-        comment TEXT
-        )
-        """
-
-        cursor.execute(create_okved_table)
-
-        connection.commit()
-
+ 
     # Загружаем файл егрюл и разархиваруем данные
-    @task(task_id='data_downloading')
-    def data_downloading(url):
-        try:
-            url = 'https://ofdata.ru/open-data/download/okved_2.json.zip'
-            filename = wget.download(url)
-            logging.info('Файл загружен')
-        except Exception as e:
-            logging.error(f'Не удаётся загрузить файл: {e}')
 
-            # Распаковываем архив
-        with ZipFile(filename, 'r') as zipobj:
-            zipobj.extractall()
-        jsonFile = 'okved_2.json'
+    create_tables = PythonOperator(
+        task_id='create_tables'
+        python_callable=run_create_db 
+    )
 
-    run_task0 = data_downloading('someurl')
-
-    task_1 = PythonOperator(
+    data_downloading = PythonOperator(
         task_id='data_downloading',
-        python_callable=data_downloading
+        python_callable=run_data_downloading
+    )
+
+    extract_okved = PythonOperator(
+        task_id='extract_okved'
+        python_callable=run_extract_okved 
+    )
+
+    vacancies_parsing= PythonOperator(
+        task_id='vacancies_parsing'
+        python_callcable=run_vacancies_parsing 
     )
 
 
+    create_tables >> data_downloading >> extract_okved
+    run_vacancies_parsing >> 
 
-    # Task dependency method 1
-    # task1.set_downstream(task2)
-    # task1.set_downstream(task3)
-
-    # Task dependency method 2
-    # task1 >> task2
-    # task1 >> task3
-
-    run_task0 >> task_1
-    # Task dependency method 3
-    # task1 >> [task2, task3]
