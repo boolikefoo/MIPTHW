fe7e049fa82f9353b9564800c4193e84ecaf643d
Artem Pixaykin <53806715+boolikefoo@users.noreply.github.com>
2023-08-17 12:50:47 +0500

    update

--- HW_03/airflow/dags/HW_03.py
+++ HW_03/airflow/dags/HW_03.py
@@ -1,6 +1,7 @@
 # импортируем библиотеки
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, sleep
 import logging
+import requests
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.python import PythonOperator
@@ -8,9 +9,10 @@ import pandas as pd
 import sqlite3
 import zipfile
 import wget
-from sqlalchemy.orm import Mapped
+import json
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
+
 
 # Настройки DAG
 default_args = {
@@ -21,6 +23,7 @@ default_args = {
 
 
 # параметры для парсинга
+url_api = 'https://api.hh.ru/vacancies'
 user_agent = {'User-agent': 'Mozilla/5.0'}
 url_params = {
     'text': 'middle python developer',
@@ -49,8 +52,8 @@ def run_data_downloading():
 # функция выбора компаний по оквэд
 def run_extract_okved():
     # Создаём подключение к DB
-    connection = sqlite3.connect('HW1.db')
-
+    connection = sqlite3.connect('sqlite:///hw3.db')
+    targetFile = 'egrul.json.zip'
     # Создаём курсор
     cursor = connection.cursor()
 
@@ -89,7 +92,7 @@ def run_extract_okved():
 # Функция создания баз данных
 def run_create_db():
     # Создаём базу данных
-    connection = sqlite3.connect('hw1.db')
+    connection = sqlite3.connect('sqlite:///hw3.db')
 
     cursor = connection.cursor()
 
@@ -121,6 +124,62 @@ def run_create_db():
 
 
 # Функция парсинга вакансий
+def run_vacancies_parsing():
+    engine = create_engine('sqlite:///hw3.db')
+    # функция получения списка ссылок для парсинга по API
+    def api_parsing():
+        data = parse_data(url_api, user_agent, url_params, mod='api')
+        data = data.get('items')
+
+        with Session(engine) as session:
+            for link in tqdm(data):
+                try:
+                    vacancy = api_parsing_vacancies(link['url'])
+                    sleep(0.2)
+                    session.add(vacancy)
+                except Exception as e:
+                    print(f'Произошла ошибка: {e}')       
+            try:
+                session.commit()
+                print('Данные успешно записаны в базу данных')
+            except Exception as e:
+                print(f'Произошла ошибка при попытке записи данных: {e}')
+
+            session.close()
+            engine.dispose()
+
+
+    # Функция парсинга вакансии через AIP
+    def api_parsing_vacancies(url):
+        vacancy = parse_data(url, user_agent, url_params, mod='api')
+
+        data = Vacancies(
+            company_name=vacancy.get('employer').get('name'),
+            position=vacancy.get('name'),
+            job_description=str(vacancy.get('description')),
+            key_skills=str(vacancy.get('key_skills'))
+        )
+
+        return data
+    
+    # Функция проверки наличия данных
+    def data_check(data):
+        try:
+            return data.text
+        except Exception as e:
+            logging.error(f'Ошибка: {e}')
+            data = 'Empty'
+            return data
+
+
+    # Проверяем ответ сервера на запрос
+    def parse_data(url, user_agent, url_params):
+        result = requests.get(url, headers=user_agent, params=url_params)
+        if result.status_code == 200:
+            data = result.json()
+            return data            
+        else:
+            logging.error(f'Сервер вернул ошибку: {result.status_code}')
 
 
 with DAG(
@@ -131,10 +190,7 @@ with DAG(
     schedule_interval='@daily
 ) as dag:
 
-    # Создаём базу данных
- 
     # Загружаем файл егрюл и разархиваруем данные
-
     create_tables = PythonOperator(
         task_id='create_tables'
         python_callable=run_create_db 
@@ -157,5 +213,5 @@ with DAG(
 
 
     create_tables >> data_downloading >> extract_okved
-    run_vacancies_parsing >> 
+    vacancies_parsing >> 
 
